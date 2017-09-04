58ccfcb2dd5d78a112d01f53a59912e7da5f8a7b
Kevin Rejko <krejko@gmail.com>
2017-09-04 09:10:46 -0400

    Moved game board view to its own xib for compartmentalization

--- TicTacToe/GameBoardView.swift
+++ TicTacToe/GameBoardView.swift
@@ -8,10 +8,48 @@
 
 import UIKit
 
+@IBDesignable
 class GameBoardView: UIView {
 
     
+    @IBOutlet var view: UIView!
+    @IBOutlet weak var leftVerticalLine: VerticalLineView!
+    @IBOutlet weak var rightVerticalLine: VerticalLineView!
+    @IBOutlet weak var topHorizontalLine: HorizontalLineView!
+    @IBOutlet weak var bottomHorizontalLine: HorizontalLineView!
+
+    // MARK: - Initialization
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setup()
+    }
+
+    func setup() {
+        view = loadViewFromNib() 
+        view.frame = bounds
+        addSubview(view)
+    }
+    
+    func loadViewFromNib() -> UIView! {
+        let bundle = Bundle(for: type(of: self))
+        let nib = UINib(nibName: String(describing: type(of: self)), bundle: bundle)
+        let view = nib.instantiate(withOwner: self, options: nil)[0] as! UIView
+        return view
+    }
+
+    // MARK: - Board Setup
+
     func drawBoard(animated: Bool) {
-        
+        let animationSpeed = CGFloat(0.15)
+        self.leftVerticalLine.drawLine(withDuration: animationSpeed)
+        self.rightVerticalLine.drawLine(withDuration: animationSpeed, delay: Double(animationSpeed))
+        self.topHorizontalLine.drawLine(withDuration: animationSpeed, delay: Double(animationSpeed * 2))
+        self.bottomHorizontalLine.drawLine(withDuration: animationSpeed, delay: Double(animationSpeed * 3))
     }
 }
--- TicTacToe/GameViewController.swift
+++ TicTacToe/GameViewController.swift
@@ -9,34 +9,22 @@
 import UIKit
 
 class GameViewController: UIViewController {
+   
     
     @IBOutlet weak var gameBoardView: GameBoardView!
-    @IBOutlet weak var leftVerticalLine: VerticalLineView!
-    @IBOutlet weak var rightVerticalLine: VerticalLineView!
-    @IBOutlet weak var topHorizontalLine: HorizontalLineView!
-    @IBOutlet weak var bottomHorizontalLine: HorizontalLineView!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
     }
     
     override func viewDidAppear(_ animated: Bool) {
-//        self.gameBoardView.drawBoard(animated: animated)
-        let animationSpeed = CGFloat(0.15)
-        self.leftVerticalLine.drawLine(withDuration: animationSpeed)
-        self.rightVerticalLine.drawLine(withDuration: animationSpeed, delay: Double(animationSpeed))
-        self.topHorizontalLine.drawLine(withDuration: animationSpeed, delay: Double(animationSpeed * 2))
-        self.bottomHorizontalLine.drawLine(withDuration: animationSpeed, delay: Double(animationSpeed * 3))
-
+        self.gameBoardView.drawBoard(animated: animated)
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-    
 }
 
